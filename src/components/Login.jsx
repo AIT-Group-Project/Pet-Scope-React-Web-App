24162d801b311a4e81d07930cac9b308dd192659
xinjalin <xinjalin@gmail.com>
2023-07-13 11:17:06 +0930

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,11 +17,11 @@ export default function Login() {
     <form className='font-semibold'>
       <label type='text'>
         <p className='dark:text-white'>Email</p>
-        <input type='email' name='log-input-email' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='email' name='log-input-email' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <label type='text'>
         <p className='dark:text-white'>Password</p>
-        <input type='password' name='log-input-password' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='password' name='log-input-password' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <div className = 'relative text-xl text-center rounded-full p-3 mt-1 hover:bg-slate-500'>
         <button type="submit" className='w-full dark:text-white'>Submit</button>
