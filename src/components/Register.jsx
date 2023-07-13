f61edd5f271f8f6ca4fe5358719b5d82d2079835
xinjalin <xinjalin@gmail.com>
2023-07-13 10:40:23 +0930

    update register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -21,19 +21,19 @@ export default function Register() {
     <form className='font-semibold'>
       <label type='text'>
         <p className='dark:text-white'>First Name</p>
-        <input type='text' name='reg-input-first-name' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='text' name='reg-input-first-name' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <label type='text'>
         <p className='dark:text-white'>Last Name</p>
-        <input type='text' name='reg-input-last-name' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='text' name='reg-input-last-name' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <label type='text'>
         <p className='dark:text-white'>Email</p>
-        <input type='email' name='reg-input-email' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='email' name='reg-input-email' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <label type='text'>
         <p className='dark:text-white'>Password</p>
-        <input type='password' name='reg-input-password' className='w-full p-1 my-1' onChange={handleInputChange} />
+        <input type='password' name='reg-input-password' className='w-full p-1 my-1' onChange={handleInputChange} required/>
       </label>
       <div className = 'relative text-xl text-center rounded-full p-3 mt-1 hover:bg-slate-500'>
         <button type="submit" className='w-full dark:text-white'>Submit</button>
