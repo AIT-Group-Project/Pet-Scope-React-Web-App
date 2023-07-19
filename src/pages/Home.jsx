26c10bca9c6ec12d4f94bf156e68dabd9cd711ed
crushedace <93542325+crushedace@users.noreply.github.com>
2023-07-19 05:43:17 +0000

    removed bg.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,2 @@
 import React from 'react'
 
-const homepage = () => {
-  return (
-
-  <div class="bg-[url(https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg)]  h-screen w-full relative">
-    <div class="absolute top-0 px-4 py-3 bg-gray-500/50 w-full">
-      <h1 class="text-white font-semibold text-4xl"> Our veterinary clinic provides modern, professional and affordable health care for your pets.
-                </h1>
-      <p class="text-gray-200">
-            We aim to make your pet feel at ease in our clinic with a caring approach to consultations and treatment.
-            Our clinic, you’ll be fully supported with sound advice by leading Veterinarians and Nurses. 
-            We know that helping you starts with providing a clear understanding of health care issues and treatment options.
-            With expert advice and professional recommendations, you’ll be well equipped to make the best health care decisions for your pet.
-      </p>
-  </div>
-</div>
-    
-  )
-}
-
-export default homepage
