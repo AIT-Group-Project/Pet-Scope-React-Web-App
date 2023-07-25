1fbd248960bfb1632ba845a1e760b1771760c888
crushedace <93542325+crushedace@users.noreply.github.com>
2023-07-25 03:16:47 +0000

    hope this works

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,32 @@
 import React from 'react'
 
+<<<<<<< HEAD
+=======
+const homepage = () => {
+  return (
+    <section className="w-full h-screen">
+
+      <img className=" top-0 left-0 w-full h-screen object-cover" src="https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg" alt="/"/>
+      <div className='w-screen h-screen fixed inset-0 bg-black/[0.2]'/>
+
+      <div className='top-0 w-full h-full flex flex-col justify-center text-white' >
+        <div className=' md:left-[10%] max-w-[1100px] m-auto p-4'>
+
+          <h1>Our veterinary clinic provides modern, professional and affordable health care for your pets.
+              We aim to make your pet feel at ease in our clinic with a caring approach to consultations and treatment. 
+          </h1>
+          
+          <p>
+            Our clinic, you’ll be fully supported with sound advice by leading Veterinarians and Nurses. 
+            We know that helping you starts with providing a clear understanding of health care issues and treatment options.
+            With expert advice and professional recommendations, you’ll be well equipped to make the best health care decisions for your pet.
+          </p>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default homepage
+>>>>>>> b3c9a2c (hope this works)
