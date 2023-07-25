61ac1a1afa7644e006380b80e2444f66e34e3e7b
crushedace <93542325+crushedace@users.noreply.github.com>
2023-07-25 05:25:23 +0000

    fixed the text issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,20 @@ import React from 'react'
 =======
 const homepage = () => {
   return (
-    <section className="w-full h-screen">
 
-      <img className=" top-0 left-0 w-full h-screen object-cover" src="https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg" alt="/"/>
-      <div className='w-screen h-screen fixed inset-0 bg-black/[0.2]'/>
-
-      <div className='top-0 w-full h-full flex flex-col justify-center text-white' >
-        <div className=' md:left-[10%] max-w-[1100px] m-auto p-4'>
-
-          <h1>Our veterinary clinic provides modern, professional and affordable health care for your pets.
-              We aim to make your pet feel at ease in our clinic with a caring approach to consultations and treatment. 
-          </h1>
-          
-          <p>
+  <div class="bg-[url(https://images.pexels.com/photos/46024/pexels-photo-46024.jpeg)] h-screen w-screen relative">
+    <div class="absolute bottom-0 px-4 py-3 bg-gray-500/50 w-full">
+      <h1 class="text-white font-semibold text-4xl"> Our veterinary clinic provides modern, professional and affordable health care for your pets.
+                </h1>
+      <p class="text-gray-200">
+            We aim to make your pet feel at ease in our clinic with a caring approach to consultations and treatment.
             Our clinic, you’ll be fully supported with sound advice by leading Veterinarians and Nurses. 
             We know that helping you starts with providing a clear understanding of health care issues and treatment options.
             With expert advice and professional recommendations, you’ll be well equipped to make the best health care decisions for your pet.
-          </p>
-
-        </div>
-      </div>
-    </section>
+      </p>
+  </div>
+</div>
+    
   )
 }
 
