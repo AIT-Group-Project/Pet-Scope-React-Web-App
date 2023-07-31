86618b441e989720bb9f68aafd7633ebb5d32b1f
xinjalin <xinjalin@gmail.com>
2023-07-31 18:14:22 +0930

    updated how index.js renders the react app to  work with react 18

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(
-    <ContextProvider>
-        <App />
-    </ContextProvider>,
-    document.getElementById('root')
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+function AppWithCallbackAfterRender() {
+  React.useEffect(() => {
+    //console.log('rendered');
+  }, []);
+
+  return <App tab="home" />;
+}
+
+root.render(
+  <ContextProvider>
+    <AppWithCallbackAfterRender />
+  </ContextProvider>
 );
