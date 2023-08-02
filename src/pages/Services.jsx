3ba5032dd6517dee2604dc447ef43f657b5c8349
DarthOhmost <cameronswiggs@gmail.com>
2023-08-02 15:18:12 +0930

    Fixed BG on Services Page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,11 +3,15 @@ import {PlayDateCalendar} from '../components'
 
 const Services = () => {
   return (
+    <div
+      class="h-screen dark:bg-slate-500">
+   
     <div className=' text-center font-semibold text-blue-700 dark:bg-slate-500 dark:text-white'> 
     Puppy Play Date App
       <PlayDateCalendar />
-      <div className='text-center p-80 mx-auto'>  
+      <div className='text-center mx-auto'>
       </div>
+    </div>
     </div>
   )
 }
