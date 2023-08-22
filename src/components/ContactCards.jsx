36c8cfa5af0b9286d489e1cd99ddc11d37e9f8d3
EnderKnightError <jarrodwilliams.1@outlook.com>
2023-08-22 15:35:57 +0930

    refining styles

--- src/components/ContactCards.jsx
+++ src/components/ContactCards.jsx
@@ -1,76 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+
 
 const ContactCards = () => {
+    const NameStyle = useState ('text-gray-700 dark:text-white font-semibold mb-2 center');
+    const MainStyle = useState ('text-center mt-6 center');
+    const FigureStyle = useState ('bg-white dark:bg-slate-700 h-80 rounded-lg shadow-md pt-7 center');
+    const Middletext = useState ('text-gray-300 dark:text-white')
   return (
     <>
-        <figure className="bg-white dark:bg-slate-700 h-80 rounded-lg shadow-md pt-7">
+        <figure className={`${FigureStyle}`}>
             <img 
                 alt="User"
                 className="w-32 h-32 rounded-full mx-auto"
                 src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
             />
-            <figcaption className='text-center mt-5'>
-                <p className='text-gray-700 dark:text-white font-semibold mb-2'>
+            <figcaption className={`${MainStyle}`}>
+                <p className={`${NameStyle}`}>
                     Addison Golden
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                    <span className='font-medium'> Email: </span> Addison@test.com
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                     <span className='font-medium'>Phone:</span> 555-555-555
                 </p>
             </figcaption>
         </figure>
-        <figure className="bg-white dark:bg-slate-700 h-80 rounded-lg shadow-md pt-7">
+        <figure className={`${FigureStyle}`}>
             <img 
                 alt="User"
                 className="w-32 h-32 rounded-full mx-auto"
                 src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
             />
-            <figcaption className='text-center mt-5'>
-                <p className='text-gray-700 dark:text-white font-semibold mb-2'>
+            <figcaption className={`${MainStyle}`}>
+                <p className={`${NameStyle}`}>
                     Addison Golden
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                    <span className='font-medium'> Email: </span> Addison@test.com
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                     <span className='font-medium'>Phone:</span> 555-555-555
                 </p>
             </figcaption>
         </figure>
-        <figure className="bg-white h-80 dark:bg-slate-700 rounded-lg shadow-md pt-7">
+        <figure className={`${FigureStyle}`}>
             <img 
                 alt="User"
                 className="w-32 h-32 rounded-full mx-auto"
                 src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
             />
-            <figcaption className='text-center mt-5'>
-                <p className='text-gray-700 dark:text-white font-semibold mb-2'>
+            <figcaption className={`${MainStyle}`}>
+                <p className={`${NameStyle}`}>
                     Addison Golden
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                    <span className='font-medium'> Email: </span> Addison@test.com
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                     <span className='font-medium'>Phone:</span> 555-555-555
                 </p>
             </figcaption>
         </figure>
-        <figure className="bg-white h-80 dark:bg-slate-700 rounded-lg shadow-md pt-7">
+        <figure className={`${FigureStyle}`}>
             <img 
                 alt="User"
                 className="w-32 h-32 rounded-full mx-auto"
                 src="https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
             />
-            <figcaption className='text-center mt-5'>
-                <p className='text-gray-700 dark:text-white font-semibold mb-2'>
+            <figcaption className={`${MainStyle}`}>
+                <p className={`${NameStyle}`}>
                     Addison Golden
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                    <span className='font-medium'> Email: </span> Addison@test.com
                 </p>
-                <p className='text-gray-500 dark:text-white'>
+                <p className={`${Middletext}`}>
                     <span className='font-medium'>Phone:</span> 555-555-555
                 </p>
             </figcaption>
