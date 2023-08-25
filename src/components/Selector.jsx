24bc222f5c2b4e8e2ec5bf82fdaecfa20c9866c3
EnderKnighterror <106501005+EnderKnighterror@users.noreply.github.com>
2023-08-25 15:45:57 +0930

    Mini edits
    More of an understanding on how dropdown menus work

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -4,18 +4,12 @@ import { useState } from 'react';
 
 const Selector = () => {
 
-  const dropdownstyle = useState('p-2 text-sm hover:bg-sky-600 hover:text-white')
+  const dropdownstyle = useState('p-2 w-10 text-sm hover:bg-sky-600 hover:text-white')
   return (
-    <select className='items-center pt-5' class="ui selection dropdown">
-  <option className={`${dropdownstyle}`} value="">Pet</option>
+    <select className='text-center pt-5 center w-32 h-32 ' class="ui selection dropdown">
+  <option className={`${dropdownstyle}`} value="">Select a Pet</option>
   <option className={`${dropdownstyle}`} value="0">Cat</option>
   <option className={`${dropdownstyle}`} value="1">Dog</option>
-  <option className={`${dropdownstyle}`} value="2">Bird</option>
-  <option className={`${dropdownstyle}`} value="3">Rabbit</option>
-  <option className={`${dropdownstyle}`} value="4">Squirrel</option>
-  <option className={`${dropdownstyle}`} value="5">Horse</option>
-  <option className={`${dropdownstyle}`} value="6">Turtle</option>
-  <option className={`${dropdownstyle}`} value="7">Parrot</option>
 </select>
 
   )
