da5ff5197bb434aa8e928c5dfc41ab05ad0ed217
EnderKnightError <jarrodwilliams.1@outlook.com>
2023-09-04 13:16:44 +0930

    Staff-page-complete

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { ContactCards } from '../components'
+import CardContainer from '../components/StaffCards/CardContainer'
 
 const Staff = () => {
   return (
     <div className='dark:bg-slate-600'>
       <section className='p-20 grid md:grid-cols-2 sm:grid-cols-1 gap-6'>
-        <ContactCards />
+        <CardContainer />
       </section>
     </div>
   )
