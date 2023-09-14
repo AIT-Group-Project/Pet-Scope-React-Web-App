a223f0f83a96cb419f200a2befc1ba65fc3ae70e
DarthOhmost <123339632+DarthOhmost@users.noreply.github.com>
2023-09-14 11:06:42 +0930

    Invites for user that they have sent are now displayed

---
+++ src/components/PlayDateInvites/PDDisplayReceivedForUser.jsx
@@ -0,0 +1,59 @@
+
+const PDDisplayReceivedForUser = (props) => {
+    const timeOptions = ['08:00 AM','09:00 AM','10:00 AM','2:00 PM','3:00 PM'];
+    const timeDatabaseFormatted = ['08:00:00','09:00:00','10:00:00','14:00:00','15:00:00'];
+
+
+    const formatDate = (date) => {
+        let newDate = new Date(date).toLocaleDateString('en-AU', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',       
+        }).split('/').join('-'); // This ensures the date format is in 'DD-MM-YYYY'
+        return newDate;
+    }
+
+    const getTwelveHourTime = (time) => {
+        const v1 = timeDatabaseFormatted.indexOf(time);
+        const v2 = timeOptions[v1]
+        return v2;
+    }
+
+    const formatTime = (time) => {
+        const newTime = time.split('T');
+        let formattedTime = newTime[1].replace('Z', '').slice(0, -4);
+        return getTwelveHourTime(formattedTime);
+    }
+
+    const formatConfirm = (confirmed) => {
+        if (confirmed === "T") {
+            confirmed = "Accepted"
+            return confirmed;
+        }
+        if (confirmed === "F") {
+            confirmed = "Declined"
+            return confirmed;
+        }
+        if (confirmed === null) {
+            confirmed = "Pending"
+            return confirmed;
+        }};
+
+  return (
+    <div className='flex'>
+        <div className='mx-auto'>
+            <div className='p-2'>
+                <p>Sender: {props.PDSenderName}</p>
+                <p>Recevier: {props.PDReceiverName}</p>
+                <p>Date: {formatDate(props.PDDate)}</p>
+                <p>Time: {formatTime(props.PDTime)}</p>
+                <p>Confirmed?: {formatConfirm(props.PDConfirmed)}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
+
+export default PDDisplayReceivedForUser;
--- src/pages/MyPlayDate.jsx
+++ src/pages/MyPlayDate.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import InviteReceiver from '../components/PlayDateInvites/InviteReceiver';
+import InviteReceiverForUser from '../components/PlayDateInvites/InviteReceiverForUser';
 import { PlayDateContextProvider } from '../contexts/PlayDateContextProvider';
 
 const MyPlayDate = () => {
 
     
     return (
-        <div className='dark:bg-slate-500'>
+        <div className='dark:bg-slate-500 grid grid-cols-1 sm:grid-cols-6'>
             <PlayDateContextProvider>
+                User Playdate Recieved
                 <InviteReceiver />
+            </PlayDateContextProvider>
+
+            <PlayDateContextProvider>
+                User Playdate Sent
+                <InviteReceiverForUser/>
             </PlayDateContextProvider>
         </div>
     )
