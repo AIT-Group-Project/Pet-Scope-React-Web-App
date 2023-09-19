ea5cc5c60009cba434a27e1b6446c3defc373c34
EnderKnightError <jarrodwilliams.1@outlook.com>
2023-09-19 16:35:08 +0930

    update adding image

--- src/components/Profile/Form.jsx
+++ src/components/Profile/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useReducer } from 'react';
+import { useState } from 'react';
 import { useContext } from 'react';
 import axios from '../../api/axios';
 import  AuthProvider  from '../../contexts/AuthProvider';
@@ -11,6 +12,8 @@ const formReducer = (state, action) => {
   switch (action.type) {
     case 'UPDATE_FIELD':
       return { ...state, [action.field]: action.value };
+    case 'UPDATE_IMAGE':
+      return {...state, image: action.value };
     case 'RESET_FORM':
       return {
         petname: '',
@@ -41,32 +44,50 @@ function Form() {
     address: '',
     phoneNo: '',
     postcode: '',
+    image: null,
   });
-  
-
+  const [imageError, setImageError] = useState('');
   
 	const handleChange = (event) => {
 	  const { name, value } = event.target;
 	  dispatch({ type: 'UPDATE_FIELD', field: name, value });
 	  console.log('formData', formData);
 	};
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.size <= 10 * 1024 * 1024) {
+        dispatch({type: 'UPDATE_IMAGE', value: file });
+        console.log(formData.image)
+        setImageError('');
+      } else {
+        setImageError('Image size exceeds 10mb limit');
+      }
+    }
+  };
   
 	const handleSubmit = async (event, UID) => {
 	  event.preventDefault();
 		console.log('formData', formData);
 	  try {
-		const response = await axios.post(PROFILE_URL,
-			{...formData, UID},
-			{ 
-				headers: {'Authorization': `Bearer ${auth.accessToken}`},
-				withCredentials: true
-			});
-		console.log('API response:', response.data);
-		dispatch({ type: 'RESET_FORM' }); // Reset the form after submission
-		formRef.current.reset();
-	  } catch (error) {
-		console.error('Error:', error);
-	  }
+      const formDataToSend = new FormData();
+      for (const key in formData) {
+        formDataToSend.append(key, formData[key]);
+      }
+      console.log([...formDataToSend.entries()]);
+		  const response = await axios.post(PROFILE_URL,
+		  	formDataToSend, UID, 
+        {
+          headers: {'Authorization': `Bearer ${auth.accessToken}`, 'Content-Type': 'multipart/form-data'},
+          withCredentials: true
+        });
+		  console.log('API response:', response.data);
+		  dispatch({ type: 'RESET_FORM' }); // Reset the form after submission
+		  formRef.current.reset();
+	    } catch (error) {
+		  console.error('Error:', error);
+	    }
 	};
   
 	return (
@@ -81,20 +102,37 @@ function Form() {
 		/>
 		<div className='mt-3'>
           <label htmlFor='dogorcat' className='block text-m font-medium leading-6 text-gray-900'>Dog or cat</label>
-          <input
-            name='dogorcat'
-            placeholder='Dog or Cat'
-			value={formData.dogorcat}
-			onChange={handleChange}
-          />
+          <div>
+            <label className='inline-flex items-center'>
+              <input
+                type='radio'
+                name='dogorcat'
+                value='dog'
+                checked={formData.dogorcat === 'dog'}
+                onChange={handleChange}
+              />
+              <span className='ml-2'>Dog</span>
+            </label>
+            <label className='inline-flex items-center ml-4'>
+              <input
+                type='radio'
+                name='dogorcat'
+                value='cat'
+                checked={formData.dogorcat === 'cat'}
+                onChange={handleChange}
+              />
+              <span className='ml-2'>Cat</span>
+            </label>
+          </div>
+
         </div>
         <div className='mt-3'>
           <label htmlFor='gender' className='block text-m font-medium leading-6 text-gray-900'>Gender</label>
           <input
             name='gender'
             placeholder='Gender'
-			value={formData.gender}
-			onChange={handleChange}
+			      value={formData.gender}
+			      onChange={handleChange}
           />
         </div>
         <div className='mt-3'>
@@ -102,8 +140,8 @@ function Form() {
           <input
             name='breed'
             placeholder='Breed'
-			value={formData.breed}
-			onChange={handleChange}
+			      value={formData.breed}
+			      onChange={handleChange}
           />
         </div>
         <div className='mt-3'>
@@ -111,8 +149,8 @@ function Form() {
           <input
             name='age'
             placeholder='Age'
-			value={formData.age}
-			onChange={handleChange}
+			      value={formData.age}
+			      onChange={handleChange}
           />
         </div>
         <div className='mt-3'>
@@ -120,8 +158,8 @@ function Form() {
           <input
             name='address'
             placeholder='Address'
-			value={formData.address}
-			onChange={handleChange}
+			      value={formData.address}
+			      onChange={handleChange}
           />
         </div>
         <div className='mt-3'>
@@ -129,8 +167,8 @@ function Form() {
           <input
             name='phoneNo'
             placeholder='Phone Number'
-			value={formData.phoneNo}
-			onChange={handleChange}
+			      value={formData.phoneNo}
+			      onChange={handleChange}
           />
         </div>
         <div className='mt-3'>
@@ -138,9 +176,21 @@ function Form() {
           <input
             name='postcode'
             placeholder='postcode'
-			value={formData.postcode}
-			onChange={handleChange}
+			      value={formData.postcode}
+			      onChange={handleChange}
           />
+        </div>
+        <div className='mt-3'>
+          <label htmlFor='image' className='block text-m font-medium leading-6 text-gray-900'>
+            Upload Image (Max 10MB)
+          </label>
+          <input
+            type='file'
+            name='image'
+            accept='image/*'
+            onChange={handleImageChange}
+          />
+          {imageError && <p className='text-red-500'>{imageError}</p>}
         </div>
 		<div className='mt-3'>
 		  <button type='submit' className='justify-center items-center mt-3 py-4'>Update Profile</button>
